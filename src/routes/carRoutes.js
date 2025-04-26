7b6def7ceb9a8de06a06fffeee509a7418548fbf
Rida-Lad <ridlad7@gmail.com>
2025-04-26 16:07:46 +0100

    chore: remove deprecated environment and database configuration files

--- src/routes/carRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getCars, getCarById } from '../controllers/carController.js';
-
-const router = express.Router();
-
-router.get('/', getCars);
-router.get('/:id', getCarById);
-
-export default router;
